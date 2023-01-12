8560ad90f5b05c4d170d09248aff0c4a6ab71fbc
Abhirakshak <abhirakshak10@gmail.com>
2023-01-12 11:21:17 -0500

    react and fastapi set up, need to connect them

---
+++ react-frontend/src/components/Message.js
@@ -0,0 +1,8 @@
+import React, { useState, useEffect } from "react";
+
+export default function Message() {
+    const [result, setResult] = useState(null);
+    
+    const message = async
+  return <div>Message</div>;
+}
