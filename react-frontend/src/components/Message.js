d9981f457421028d68a7d286e76aa57f0177ba31
Abhirakshak <abhirakshak10@gmail.com>
2023-01-13 11:39:38 -0500

    connected frontend and backend with axios

--- react-frontend/src/components/Message.js
+++ react-frontend/src/components/Message.js
@@ -1,8 +1,21 @@
 import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 export default function Message() {
-    const [result, setResult] = useState(null);
-    
-    const message = async
-  return <div>Message</div>;
+  const [result, setResult] = useState(null);
+
+  const message = async () => {
+    try {
+      let res = await axios.get("http://127.0.0.1:8000");
+      let result = res.data;
+      setResult(result);
+    } catch (e) {
+      console.log(e);
+    }
+  };
+  useEffect(() => {
+    message();
+  }, []);
+
+  return <div>{result}</div>;
 }
