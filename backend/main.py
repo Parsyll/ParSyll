066aedf27890fab83ce9cee776c765b62ad14979
pongpatapee <pongpatapee1021@gmail.com>
2023-01-17 14:33:30 -0500

    Fix: fixed error 422 on pdfsubmit endpoint

--- backend/main.py
+++ backend/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, UploadFile, File
 from starlette.middleware.cors import CORSMiddleware
 
 app = FastAPI()
@@ -12,12 +12,17 @@ app.add_middleware(
     allow_origins=origins,
     allow_credentials=True,
     allow_methods=['GET', 'POST'],
-    allow_headers=['Content-Type', 'application/xml', "application/pdf."],
+    allow_headers=['Content-Type', 'application/xml', "application/pdf", "multipart/form-data"],
 )
 @app.get("/")
 async def root():
     return "Hello, welcome to Parsyll!"
 
+
 @app.post("/pdfsubmit")
-async def get_pdf(pdfFile):
-    print(type(pdfFile))
+async def upload_file(file: UploadFile):
+    #useful links: 
+    # https://stackoverflow.com/questions/64168340/how-to-send-a-file-docx-doc-pdf-or-json-to-fastapi-and-predict-on-it-without
+    # https://fastapi.tiangolo.com/tutorial/request-files/
+
+    return {"filename": file.filename}
