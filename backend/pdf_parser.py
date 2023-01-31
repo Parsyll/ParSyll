65fb8e837d1a92237ff6faa4b53bcf5a20f0c6b3
Abhirakshak <abhirakshak10@gmail.com>
2023-01-30 19:46:25 -0500

    feat: elementary level of gpt parsing works and correct ics file is generated.                                                                                                                                                     Need to make many improvements to the parsing logic, for example: (1) need to make it add recurring events, (2) multiple lecture timings not supported yet, (3) office hours and textbook not supported yet, etc.

--- backend/pdf_parser.py
+++ backend/pdf_parser.py
@@ -1,9 +1,46 @@
 import PyPDF2
 import openai
 import os
+import wandb
 
+# from dotenv import load_dotenv
+
+# load_dotenv()
+
+# Information needed:
+# {
+#     course_num: IE314,
+#     class_timings: [Tuesday, 3:30pm, 60minutes], [Thursday, 6:00pm, 60minutes],
+#     location: EE129,
+#     instructors: 
+#     {
+#         {
+#             name : Prof Davis,
+#             office_hours : [Tuesday, 4:30-5:30pm], [Thursday, 5:30-6:30pm],
+#             office_location: EE 214
+#         }
+#     },
+#     teaching_assistants:
+#     {
+#         {
+#             name: Ibrahim,
+#             office_hours: [Tuesday, 4:30-5:30pm], [Thursday, 5:30-6:30pm],
+#             office_location: ARMS 1098
+#         },
+#         {
+#             name: Seungkeun,
+#             office_hours: [Tuesday, 4:30-5:30pm],
+#             office_location: ARMS 2134
+#         }
+
+#     },
+#     textbook: "Introduction to Real Analysis 4th Edition",
+# }
 
 def parse_gpt(pdfFile, promptFile):
+    # run = wandb.init(project='Parsyll GPT Parsing')
+    # prediction_table = wandb.Table(columns=["prompt", "completion"])
+
     pdf_file = open(pdfFile, 'rb')
 
     reader = PyPDF2.PdfReader(pdf_file)
@@ -13,23 +50,27 @@ def parse_gpt(pdfFile, promptFile):
         page_text = page.extract_text()
         # print(page_text)
     
-        # openai.api_key = os.getenv("OPENAI_API_KEY")
-        openai.api_key = "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"
+        openai.api_key = os.getenv("OPENAI_API_KEY")
 
 
         with open(promptFile, 'r') as file:
             prompt_text = file.read().replace('\n', '')
 
+        gpt_prompt = page_text + prompt_text
         response = openai.Completion.create(
                 model="text-davinci-003",
-                prompt=f'{page_text} {prompt_text}', 
+                prompt=f'{gpt_prompt}', 
                 max_tokens=1250,
-                temperature=0.7,
+                temperature=0.3,
                 # stream=True
             )
+        # prediction_table.add_data(gpt_prompt, response['choices'][0]['text'])
         # parse next page only if not found on this page (class timings)
         if response != "0":
             break
+
+    # wandb.log({'predictions': prediction_table})
+    # wandb.finish()
 
     print(response.choices[0].text)
     pdf_file.close()
@@ -39,4 +80,3 @@ def parse_gpt(pdfFile, promptFile):
 
 
 # parse_gpt('./etc/ie335_syllabus.pdf', 'prompts/class_timings2.txt')
-# parse_gpt('./etc/proposal.pdf')
