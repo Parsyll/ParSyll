bfa578f8535200823054a784399f3e9f75838ea9
pongpatapee <pongpatapee1021@gmail.com>
2023-02-09 10:57:22 -0500

    #14 feat: download file associated with user

--- backend/routers/pdfs.py
+++ backend/routers/pdfs.py
@@ -11,51 +11,85 @@ router = APIRouter(
     tags=["pdfs"]
 )
 
-
+# This endpoint can be used to download any file
 @router.get("/{file_id}")
 async def get_file(file_id: str):
-    blob = bucket.get_blob(file_id)
-    contents = blob.download_as_bytes()
+    try:
+        blob = bucket.get_blob(file_id)
+        contents = blob.download_as_bytes()
 
-    filename = file_id
-    if 'filename' in blob.metadata:
-        filename = blob.metadata['filename']
-    else:
-        file_ext = mimetypes.guess_extension(blob.content_type)
-        filename += file_ext
+        # filename for pdf download
+        filename = file_id
+        if 'filename' in blob.metadata:
+            filename = blob.metadata['filename']
+        else:
+            file_ext = mimetypes.guess_extension(blob.content_type)
+            filename += file_ext
+    
+    except Exception as e:
+        print(f"Error: {e}")
+        raise HTTPException(500, detail="Something went wrong")
 
     return Response(content=contents, media_type=blob.content_type, headers={"Content-Disposition": f"attachment;filename={filename}"})
 
 
+# This endpoint can only download file user:uid owns
 # user download file (should only allow download for files associated with user)
+@router.get("/{uid}/{file_id}")
+async def user_get_file(uid: str, file_id: str):
+    try:
+        user_doc_ref = db.collection(u'users').document(uid)
+        user_doc = user_doc_ref.get()
+
+        if not user_doc.exists:
+            raise HTTPException(404, detail=f"User {uid} does not exist")
+
+        user_syllabus_list = get_user_syllabus(user_doc_ref)
+        print(user_syllabus_list)
+
+        if file_id not in user_syllabus_list:
+            raise HTTPException(404, detail=f"File {file_id} does not exist for user {uid}")
+        
+        blob = bucket.get_blob(file_id)
+        contents = blob.download_as_bytes()
+
+        # filename for pdf download
+        filename = file_id
+        if 'filename' in blob.metadata:
+            filename = blob.metadata['filename']
+        else:
+            file_ext = mimetypes.guess_extension(blob.content_type)
+            filename += file_ext
+    
+    except HTTPException as e:
+        raise HTTPException(e.status_code, detail=e.detail)
+
+    except Exception as e:
+        print(f"Error: {e}")
+        raise HTTPException(500, detail="Something went wrong")
+    
+    return Response(content=contents, media_type=blob.content_type, headers={"Content-Disposition": f"attachment;filename={filename}"})
+
 
 # user upload file
 @router.post("/submit/{uid}")
 async def user_upload_file(uid: str, file: UploadFile):
-    # To link with users i guess we just put the public URL into firestore
     try:
         user = auth.get_user(uid)
         user_doc_ref = db.collection(u'users').document(user.uid)
         user_doc = user_doc_ref.get()
 
         if not user_doc.exists:
-            pass #some error handling
+            raise HTTPException(404, detail=f"User {uid} does not exist")
 
         file_contents = await file.read() 
         file_id = uuid4()
         blob = bucket.blob(str(file_id))
+        # add user in metadata to associate file with user
         blob.metadata = {'Content-Type': file.content_type, 'filename': file.filename, 'user': user.uid}
         blob.upload_from_string(file_contents, content_type=file.content_type)
 
-        # get user's current syllabus list
-        user_syllabus_ref = user_doc_ref.get(field_paths={'syllabus'})
-        user_syllabus_dict = user_syllabus_ref.to_dict()
-        print(user_syllabus_dict)
-
-        if user_syllabus_dict and 'syllabus' in user_syllabus_dict and user_syllabus_dict['syllabus']:
-            user_syllabus_list = user_syllabus_dict['syllabus']
-        else:
-            user_syllabus_list = []
+        user_syllabus_list = get_user_syllabus(user_doc_ref)
 
         print(user_syllabus_list)
         user_syllabus_list.append(str(file_id))
@@ -66,6 +100,8 @@ async def user_upload_file(uid: str, file: UploadFile):
         })
 
         
+    except HTTPException as e:
+        raise HTTPException(e.status_code, detail=e.detail)
 
     except auth.UserNotFoundError:
         raise HTTPException(404, detail=f"User {uid} not found")
@@ -77,7 +113,7 @@ async def user_upload_file(uid: str, file: UploadFile):
     return f"Uploaded {file.filename} for user {user.uid}"
 
 
-# pdf submission endpoint
+# upload file not associated to any user
 @router.post("/submit")
 async def upload_file(file: UploadFile):
     #useful links: 
@@ -92,3 +128,17 @@ async def upload_file(file: UploadFile):
     print(blob.public_url, blob.metadata)
 
     return f"File: {file.filename} uploaded as {str(file_id)}"    
+
+# Helper functions
+
+def get_user_syllabus(user_doc_ref):
+    # get user's current syllabus list
+    user_syllabus_ref = user_doc_ref.get(field_paths={'syllabus'})
+    user_syllabus_dict = user_syllabus_ref.to_dict()
+
+    if user_syllabus_dict and 'syllabus' in user_syllabus_dict and user_syllabus_dict['syllabus']:
+        user_syllabus_list = user_syllabus_dict['syllabus']
+    else:
+        user_syllabus_list = []
+    
+    return user_syllabus_list
