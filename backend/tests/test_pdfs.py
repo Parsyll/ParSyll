7ef6b495efcdb3e805084299e0c1c460c5185762
werunm <cankennethwong@gmail.com>
2023-02-23 17:42:33 -0500

    refactor: refactored tests to use fastapi.testclient

--- backend/tests/test_pdfs.py
+++ backend/tests/test_pdfs.py
@@ -7,46 +7,35 @@ import os
 import sys
 from dotenv import load_dotenv
 from parsyll_fastapi.auth.auth_handler import decodeJWT
+from fastapi.testclient import TestClient
+from parsyll_fastapi.main import app
 
 load_dotenv()
 
 admin_jwt_token = os.getenv("admin_jwt_token")
 admin_uid = os.getenv("admin_uid")
 
+header = {'Authorization': f'Bearer {admin_jwt_token}'}
+client = TestClient(app)
+
+file_path = os.path.join(os.getcwd(),  "backend", "tests", "ie335_syllabus.pdf")
 
 class TestPDFSubmission:
-    def test_pdf_submission_success(self):
-        url = "http://localhost:8000/pdfs/submit"
 
-        header = {
-            'Authorization': f'Bearer {admin_jwt_token}'
-        }
-        file_path = os.path.join(os.getcwd(), "tests", "ie335_syllabus.pdf")
+    def test_pdf_submission_success(self):
         files = {'file': open(file_path, "rb")}
         
-        response = requests.post(url, files=files, headers=header)
+        response = client.post("/pdfs/submit", headers=header, files=files)
         assert response.status_code == 200
     
 
     def test_pdf_submission_no_auth(self):
-        url = "http://localhost:8000/pdfs/submit"
-
-        header = {
-            'Authorization': f'Bearer esto-no-funciona'
-        }
-        file_path = os.path.join(os.getcwd(), "tests", "ie335_syllabus.pdf")
         files = {'file': open(file_path, "rb")}
         
-        response = requests.post(url, files=files, headers=header)
+        response = client.post("/pdfs/submit", files=files)
         assert response.status_code == 403
 
     
     def test_pdf_submission_no_file(self):
-        url = "http://localhost:8000/pdfs/submit"
-
-        header = {
-            'Authorization': f'Bearer {admin_jwt_token}'
-        }
-
-        response = requests.post(url, headers=header)
+        response = client.post("/pdfs/submit", headers=header)
         assert response.status_code == 422
