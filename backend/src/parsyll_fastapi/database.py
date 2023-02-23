e51e7354df2d7a0f1fe45bb4749473a3385d38af
pongpatapee <pongpatapee1021@gmail.com>
2023-02-23 10:16:44 -0500

    made path to json files more robust with os.path

--- backend/src/parsyll_fastapi/database.py
+++ backend/src/parsyll_fastapi/database.py
@@ -1,12 +1,13 @@
 import firebase_admin
 import pyrebase
 import json
+import os
 
 from firebase_admin import credentials, auth, firestore, storage
 
-cred = credentials.Certificate("service_account.json")
+cred = credentials.Certificate(os.path.join(os.path.dirname(__file__), "service_account.json"))
 firebase_admin.initialize_app(cred, {'storageBucket': 'parsyll.appspot.com'})
-config = json.load(open('firebase_config.json'))
+config = json.load(open(os.path.join(os.path.dirname(__file__), "firebase_config.json")))
 pb = pyrebase.initialize_app(config)
 db = firestore.client()
 bucket = storage.bucket()
