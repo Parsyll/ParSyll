11a61415d72046966bbe1861d5f606d8a37ed0b7
werunm <cankennethwong@gmail.com>
2023-03-05 22:38:49 -0500

    feat: mmm

--- backend/src/parsyll_fastapi/database.py
+++ backend/src/parsyll_fastapi/database.py
@@ -9,10 +9,15 @@ from dotenv import load_dotenv
 load_dotenv()
 
 # cred3312412343434313232
-print(os.getenv("FIREBASE_SERVICE_ACCOUNT") + "joe")
-print(os.getenv("FIREBASE_CONFIG") + "joe")
-cred = json.loads(os.getenv("FIREBASE_SERVICE_ACCOUNT"))
-config = json.loads(os.getenv("FIREBASE_CONFIG"))
+
+cred = str(os.getenv("FIREBASE_SERVICE_ACCOUNT")).strip("'<>() ").replace('\'', '\"')
+config = str(os.getenv("FIREBASE_CONFIG")).strip("'<>() ").replace('\'', '\"')
+
+print(cred)
+print(config)
+
+cred = json.loads(cred)
+config = json.loads(config)
 
 
 cred = credentials.Certificate(cred)
