eb61022a7d58d94ca4ad029b30d55846b9fa0158
pongpatapee <pongpatapee1021@gmail.com>
2023-03-05 22:13:30 -0500

    hero component for home page

--- react-frontend/src/pages/HomePage.js
+++ react-frontend/src/pages/HomePage.js
@@ -1,24 +1,12 @@
-import loadingGif from '../assets/loading_running.gif'
+import loadingGif from "../assets/loading_running.gif";
+import Hero from "../components/homePage/Hero";
 
+// Using components from: https://tailblocks.cc/
 
 const HomePage = () => {
     return (
-        <div className="flex flex-col justify-center items-center">
-            
-            <div className='flex flex-col justify-center items-center'>
-                <h1 className='text-4xl font-bold'>
-                    Welcome to ParSyll!
-                </h1>
-
-                <p className="max-w-lg">
-                    ParSyll is a webapp aimed to automate your start of the semester syllabus reading!
-                    We will automatically parse key information form your syllabus with AI tools such as ChatGPT :D 
-                </p>
-            </div>
-            
-            {/* <img src={loadingGif} alt="loading..." /> */}
-        </div>
+        <Hero />
     )
-}
+};
 
 export default HomePage;
