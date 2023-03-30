b5f495eb5b399acb4f6926d87df3dc9e002059c3
werunm <cankennethwong@gmail.com>
2023-03-30 11:05:54 -0400

    docker images for react frontend and fastapi backend

--- backend/src/parsyll_fastapi/main.py
+++ backend/src/parsyll_fastapi/main.py
@@ -48,7 +48,7 @@ async def root():
     return "Hello, welcome to Parsyll!"
 
 if __name__ == "__main__":
-   uvicorn.run("main:app", reload=True)
+   uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
 
 
 
--- backend/src/parsyll_fastapi/parsing/parser_class.py
+++ backend/src/parsyll_fastapi/parsing/parser_class.py
@@ -23,7 +23,16 @@ load_dotenv()  # take environment variables from .env.
 # except AttributeError:
 #     pass
 # else:
-#     ssl._create_default_https_context = _create_unverified_https_context
+#     ssl._create_deCMDfault_https_context = _create_unverified_https_context
+try:
+    nltk.data.find('tokenizers/punkt')
+except LookupError:
+    nltk.download('punkt')
+
+try:
+    nltk.data.find('tokenizers/stopwords')
+except LookupError:
+    nltk.download('stopwords')
 # nltk.download('punkt')
 # nltk.download('stopwords')
 
@@ -206,8 +215,7 @@ class Parser():
                 end_time = start_date.strftime('%Y-%m-%d') + ' ' + end_time[0] + ' ' + end_time[1]
 
 
-                # TODO: Issue with timezone settings, need to add 5 hours right now since
-                # EST is 5 hours behind UTC
+                # TODO: Hardcoding timezone, NEED TO FIX
                 start_time = datetime.strptime(start_time, format) + timedelta(hours=5) 
                 end_time = datetime.strptime(end_time, format) + timedelta(hours=5) 
 
