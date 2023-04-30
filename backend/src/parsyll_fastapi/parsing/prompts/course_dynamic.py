bb28311d2c13b2b8de91d17e01b970caf03eaa7b
pongpatapee <pongpatapee1021@gmail.com>
2023-04-30 08:29:32 -0400

    adjusted prompt to better parse TAs and appropriately label class timings to one of [lec, rec, lab, oh]

--- backend/src/parsyll_fastapi/parsing/prompts/course_dynamic.py
+++ backend/src/parsyll_fastapi/parsing/prompts/course_dynamic.py
@@ -4,6 +4,7 @@ import textwrap
 
 
 instructor = Person(name="Santiago", email="santiago@purdue.edu", isProf=True)
+TA = Person(name="Teaching Assistant", email="TA@purdue.edu", isProf=False)
 
 lec1 = Timing(
     location="EE 117",
@@ -25,7 +26,7 @@ textbook = Misc(tag='textbook', value='how to code 101')
 
 course_example = CourseBase(
     name="ECE 49595O",
-    instructors=[instructor],
+    instructors=[instructor, TA],
     syllabus=str(uuid4()),
     class_times=[lec1, lec2],
     school="Purdue University",
@@ -43,7 +44,9 @@ prompt_text = (
         In a format like this example:
         {course_example.dict()}
 
-        If multiple days of week are detected, please append a new entry to the 'class_times' key
+        If multiple days of week are detected, please append a new entry to the 'class_times' key.
+        Please also limit the class_time attributes to "lec", "rec", "lab", and "oh". 
+        "lec" = "lecture", "rec" = "recitation", "lab" = "lab", and "oh" = "office hours".
 
         Give only this information, no other wording or explanation please. 
     '''
